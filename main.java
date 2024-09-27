c544c254b74c123957d200b6b4868a41546f0a7c
Samarth-DevTools <samarthm@devtools.in>
2024-09-27 13:15:53 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String message = "Hello, World!";
+        for (int i = 0; i < 5; i++) {
+            System.out.println(message);
+        }
+    }
+}
